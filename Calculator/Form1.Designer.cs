f12602185252d6df4072d53473b7ee188723fb52
EavenCS <104131718+EavenCS@users.noreply.github.com>
2022-07-26 11:35:02 +0200

    Add files via upload

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace _107860
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zahl2 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.plus = new System.Windows.Forms.Button();
+            this.zahl5 = new System.Windows.Forms.Button();
+            this.zahl6 = new System.Windows.Forms.Button();
+            this.zahl1 = new System.Windows.Forms.Button();
+            this.zahl3 = new System.Windows.Forms.Button();
+            this.zahl4 = new System.Windows.Forms.Button();
+            this.zahl8 = new System.Windows.Forms.Button();
+            this.zahl7 = new System.Windows.Forms.Button();
+            this.zahl9 = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.zahl0 = new System.Windows.Forms.Button();
+            this.mal = new System.Windows.Forms.Button();
+            this.komma = new System.Windows.Forms.Button();
+            this.NegativZahlen = new System.Windows.Forms.Button();
+            this.gleichheitszeichen = new System.Windows.Forms.Button();
+            this.eineZahlZurück = new System.Windows.Forms.Button();
+            this.eintragLöschen = new System.Windows.Forms.Button();
+            this.löschen = new System.Windows.Forms.Button();
+            this.geteilt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zahl2
+            // 
+            this.zahl2.Location = new System.Drawing.Point(58, 233);
+            this.zahl2.Name = "zahl2";
+            this.zahl2.Size = new System.Drawing.Size(40, 40);
+            this.zahl2.TabIndex = 9;
+            this.zahl2.Text = "2";
+            this.zahl2.UseVisualStyleBackColor = true;
+            this.zahl2.Click += new System.EventHandler(this.zahl_click);
+            // 
+            // textBox
+            // 
+            this.textBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.Location = new System.Drawing.Point(12, 28);
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.ShortcutsEnabled = false;
+            this.textBox.Size = new System.Drawing.Size(178, 29);
+            this.textBox.TabIndex = 24;
+            this.textBox.Text = "0";
+            this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(150, 233);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(40, 40);
+            this.plus.TabIndex = 25;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // zahl5
+            // 
+            this.zahl5.Location = new System.Drawing.Point(58, 187);
+            this.zahl5.Name = "zahl5";
+            this.zahl5.Size = new System.Drawing.Size(40, 40);
+            this.zahl5.TabIndex = 26;
+            this.zahl5.Text = "5";
+            this.zahl5.UseVisualStyleBackColor = true;
+            this.zahl5.Click += new System.EventHandler(this.zahl_click);
+            // 
+            // zahl6
+            // 
+            this.zahl6.Location = new System.Drawing.Point(104, 187);
+            this.zahl6.Name = "zahl6";
+            this.zahl6.Size = new System.Drawing.Size(40, 40);
+            this.zahl6.TabIndex = 27;
+            this.zahl6.Text = "6";
+            this.zahl6.UseVisualStyleBackColor = true;
+            this.zahl6.Click += new System.EventHandler(this.zahl_click);
+            // 
+            // zahl1
+            // 
+            this.zahl1.Location = new System.Drawing.Point(12, 233);
+            this.zahl1.Name = "zahl1";
+            this.zahl1.Size = new System.Drawing.Size(40, 40);
+            this.zahl1.TabIndex = 28;
+            this.zahl1.Text = "1";
+            this.zahl1.UseVisualStyleBackColor = true;
+            this.zahl1.Click += new System.EventHandler(this.zahl_click);
+            // 
+            // zahl3
+            // 
+            this.zahl3.Location = new System.Drawing.Point(104, 233);
+            this.zahl3.Name = "zahl3";
+            this.zahl3.Size = new System.Drawing.Size(40, 40);
+            this.zahl3.TabIndex = 29;
+            this.zahl3.Text = "3";
+            this.zahl3.UseVisualStyleBackColor = true;
+            this.zahl3.Click += new System.EventHandler(this.zahl_click);
+            // 
+            // zahl4
+            // 
+            this.zahl4.Location = new System.Drawing.Point(12, 187);
+            this.zahl4.Name = "zahl4";
+            this.zahl4.Size = new System.Drawing.Size(40, 40);
+            this.zahl4.TabIndex = 30;
+            this.zahl4.Text = "4";
+            this.zahl4.UseVisualStyleBackColor = true;
+            this.zahl4.Click += new System.EventHandler(this.zahl_click);
+            // 
+            // zahl8
+            // 
+            this.zahl8.Location = new System.Drawing.Point(58, 141);
+            this.zahl8.Name = "zahl8";
+            this.zahl8.Size = new System.Drawing.Size(40, 40);
+            this.zahl8.TabIndex = 31;
+            this.zahl8.Text = "8";
+            this.zahl8.UseVisualStyleBackColor = true;
+            this.zahl8.Click += new System.EventHandler(this.zahl_click);
+            // 
+            // zahl7
+            // 
+            this.zahl7.Location = new System.Drawing.Point(12, 141);
+            this.zahl7.Name = "zahl7";
+            this.zahl7.Size = new System.Drawing.Size(40, 40);
+            this.zahl7.TabIndex = 32;
+            this.zahl7.Text = "7";
+            this.zahl7.UseVisualStyleBackColor = true;
+            this.zahl7.Click += new System.EventHandler(this.zahl_click);
+            // 
+            // zahl9
+            // 
+            this.zahl9.Location = new System.Drawing.Point(104, 141);
+            this.zahl9.Name = "zahl9";
+            this.zahl9.Size = new System.Drawing.Size(40, 40);
+            this.zahl9.TabIndex = 33;
+            this.zahl9.Text = "9";
+            this.zahl9.UseVisualStyleBackColor = true;
+            this.zahl9.Click += new System.EventHandler(this.zahl_click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(150, 187);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(40, 40);
+            this.minus.TabIndex = 34;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // zahl0
+            // 
+            this.zahl0.Location = new System.Drawing.Point(58, 279);
+            this.zahl0.Name = "zahl0";
+            this.zahl0.Size = new System.Drawing.Size(40, 40);
+            this.zahl0.TabIndex = 35;
+            this.zahl0.Text = "0";
+            this.zahl0.UseVisualStyleBackColor = true;
+            this.zahl0.Click += new System.EventHandler(this.zahl_click);
+            // 
+            // mal
+            // 
+            this.mal.Location = new System.Drawing.Point(150, 141);
+            this.mal.Name = "mal";
+            this.mal.Size = new System.Drawing.Size(40, 40);
+            this.mal.TabIndex = 36;
+            this.mal.Text = "*";
+            this.mal.UseVisualStyleBackColor = true;
+            this.mal.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // komma
+            // 
+            this.komma.Location = new System.Drawing.Point(104, 279);
+            this.komma.Name = "komma";
+            this.komma.Size = new System.Drawing.Size(40, 40);
+            this.komma.TabIndex = 37;
+            this.komma.Text = ",";
+            this.komma.UseVisualStyleBackColor = true;
+            this.komma.Click += new System.EventHandler(this.komma_Click);
+            // 
+            // NegativZahlen
+            // 
+            this.NegativZahlen.Location = new System.Drawing.Point(12, 279);
+            this.NegativZahlen.Name = "NegativZahlen";
+            this.NegativZahlen.Size = new System.Drawing.Size(40, 40);
+            this.NegativZahlen.TabIndex = 38;
+            this.NegativZahlen.Text = "+/-";
+            this.NegativZahlen.UseVisualStyleBackColor = true;
+            this.NegativZahlen.Click += new System.EventHandler(this.NegativZahlen_Click);
+            // 
+            // gleichheitszeichen
+            // 
+            this.gleichheitszeichen.Location = new System.Drawing.Point(150, 279);
+            this.gleichheitszeichen.Name = "gleichheitszeichen";
+            this.gleichheitszeichen.Size = new System.Drawing.Size(40, 40);
+            this.gleichheitszeichen.TabIndex = 39;
+            this.gleichheitszeichen.Text = "=";
+            this.gleichheitszeichen.UseVisualStyleBackColor = true;
+            this.gleichheitszeichen.Click += new System.EventHandler(this.gleichheitszeichen_Click);
+            // 
+            // eineZahlZurück
+            // 
+            this.eineZahlZurück.Location = new System.Drawing.Point(12, 95);
+            this.eineZahlZurück.Name = "eineZahlZurück";
+            this.eineZahlZurück.Size = new System.Drawing.Size(40, 40);
+            this.eineZahlZurück.TabIndex = 40;
+            this.eineZahlZurück.Text = "←";
+            this.eineZahlZurück.UseVisualStyleBackColor = true;
+            this.eineZahlZurück.Click += new System.EventHandler(this.eineZahlZurück_Click);
+            // 
+            // eintragLöschen
+            // 
+            this.eintragLöschen.Location = new System.Drawing.Point(58, 95);
+            this.eintragLöschen.Name = "eintragLöschen";
+            this.eintragLöschen.Size = new System.Drawing.Size(40, 40);
+            this.eintragLöschen.TabIndex = 41;
+            this.eintragLöschen.Text = "CE";
+            this.eintragLöschen.UseVisualStyleBackColor = true;
+            // 
+            // löschen
+            // 
+            this.löschen.Location = new System.Drawing.Point(104, 95);
+            this.löschen.Name = "löschen";
+            this.löschen.Size = new System.Drawing.Size(40, 40);
+            this.löschen.TabIndex = 42;
+            this.löschen.Text = "C";
+            this.löschen.UseVisualStyleBackColor = true;
+            this.löschen.Click += new System.EventHandler(this.löschen_Click);
+            // 
+            // geteilt
+            // 
+            this.geteilt.Location = new System.Drawing.Point(150, 95);
+            this.geteilt.Name = "geteilt";
+            this.geteilt.Size = new System.Drawing.Size(40, 40);
+            this.geteilt.TabIndex = 43;
+            this.geteilt.Text = "/";
+            this.geteilt.UseVisualStyleBackColor = true;
+            this.geteilt.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(212, 352);
+            this.Controls.Add(this.geteilt);
+            this.Controls.Add(this.löschen);
+            this.Controls.Add(this.eintragLöschen);
+            this.Controls.Add(this.eineZahlZurück);
+            this.Controls.Add(this.gleichheitszeichen);
+            this.Controls.Add(this.NegativZahlen);
+            this.Controls.Add(this.komma);
+            this.Controls.Add(this.mal);
+            this.Controls.Add(this.zahl0);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.zahl9);
+            this.Controls.Add(this.zahl7);
+            this.Controls.Add(this.zahl8);
+            this.Controls.Add(this.zahl4);
+            this.Controls.Add(this.zahl3);
+            this.Controls.Add(this.zahl1);
+            this.Controls.Add(this.zahl6);
+            this.Controls.Add(this.zahl5);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.zahl2);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Taschenrechner";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button zahl2;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button zahl5;
+        private System.Windows.Forms.Button zahl6;
+        private System.Windows.Forms.Button zahl1;
+        private System.Windows.Forms.Button zahl3;
+        private System.Windows.Forms.Button zahl4;
+        private System.Windows.Forms.Button zahl8;
+        private System.Windows.Forms.Button zahl7;
+        private System.Windows.Forms.Button zahl9;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button zahl0;
+        private System.Windows.Forms.Button mal;
+        private System.Windows.Forms.Button komma;
+        private System.Windows.Forms.Button NegativZahlen;
+        private System.Windows.Forms.Button gleichheitszeichen;
+        private System.Windows.Forms.Button eineZahlZurück;
+        private System.Windows.Forms.Button eintragLöschen;
+        private System.Windows.Forms.Button löschen;
+        private System.Windows.Forms.Button geteilt;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _107860
+{
+    public partial class Form1 : Form
+    {  
+        string Operation;
+        double ErsteNummer, ZweiteNummer, Ergebnis;
+        bool GleichzeichenRichtig, OperatorGedrückt; 
+        public Form1()
+        {       
+            InitializeComponent();
+        }
+
+        public void zahl_click(object sender, EventArgs e)
+        {
+            if (textBox.Text == "0")
+            {
+                textBox.Text = (sender as Button).Text;
+            }
+            else if (textBox.Text == "Teilen durch 0 ist nicht möglich")
+            {
+               löschen.PerformClick();
+               textBox.Text = (sender as Button).Text;
+            }
+            else
+            {
+                textBox.Text += (sender as Button).Text;
+            }       
+        }
+        public void Benable(bool b_enable)
+        {
+            mal.Enabled = b_enable;
+            plus.Enabled = b_enable;
+            minus.Enabled = b_enable;
+            geteilt.Enabled = b_enable;
+            gleichheitszeichen.Enabled = b_enable;
+        }      
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Operation = (sender as Button).Text;
+
+            if (OperatorGedrückt == false)
+            {
+                ErsteNummer = Convert.ToDouble(textBox.Text);
+                textBox.Text = "0";
+                OperatorGedrückt = true;
+            }
+                      
+        }      
+        private void gleichheitszeichen_Click(object sender, EventArgs e)
+        {           
+            if (GleichzeichenRichtig 
+                && OperatorGedrückt == false)
+            {              
+                switch (Operation)
+                {
+                    case "+":
+                        Ergebnis += ZweiteNummer;
+                        break;
+                    case "-":
+                        Ergebnis -= ZweiteNummer;
+                        break;                 
+                    case "*":
+                        Ergebnis *= ZweiteNummer;
+                        break;
+                    case "/":
+                        Ergebnis /= ZweiteNummer;                                            
+                        break;
+                }
+                textBox.Text = Convert.ToString(Ergebnis);
+            }
+            else 
+            {
+                ZweiteNummer = Convert.ToDouble(textBox.Text);
+                GleichzeichenRichtig = true;
+                switch (Operation)
+                {
+                    case "+":
+                        Ergebnis = ErsteNummer + ZweiteNummer;
+                        textBox.Text = Convert.ToString(Ergebnis);
+                        break;
+                    case "*":
+                        Ergebnis = ErsteNummer * ZweiteNummer;
+                        textBox.Text = Convert.ToString(Ergebnis);
+                        break;
+                    case "/":
+                        if (ZweiteNummer == 0)
+                        {
+                            textBox.Text = "Teilen durch 0 ist nicht möglich";
+                            Benable(false);
+                        }
+                        else
+                        {
+                            Ergebnis = ErsteNummer / ZweiteNummer;
+                            textBox.Text = Convert.ToString(Ergebnis);
+                        }
+                        break;
+                    case "-":
+                        Ergebnis = ErsteNummer - ZweiteNummer;
+                        textBox.Text = Convert.ToString(Ergebnis);
+                        break;
+                }
+            } 
+            OperatorGedrückt = false;
+        }   
+               
+        private void komma_Click(object sender, EventArgs e)
+        {
+            if (!textBox.Text.Contains("."))
+            {
+                textBox.Text += ".";
+            }
+        }
+
+        private void eineZahlZurück_Click(object sender, EventArgs e)
+        {
+           textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1, 1);           
+        }
+        private void löschen_Click(object sender, EventArgs e)
+        {
+            OperatorGedrückt = false;
+            GleichzeichenRichtig = false;
+            textBox.Text = "0";
+            Ergebnis = 0;
+            ErsteNummer = 0;
+            ZweiteNummer = 0;
+            Benable(true);
+        }
+       
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox.Text.Length == 0) 
+            {
+                textBox.Text = "0";
+            }
+        }
+
+        private void NegativZahlen_Click(object sender, EventArgs e)
+        {
+            double Negativ = Convert.ToDouble(textBox.Text) * -1;
+            textBox.Text = Convert.ToString(Negativ);         
+        }
+    }
+}
